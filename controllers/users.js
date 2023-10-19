bcc626baad619cc918649d53e0bafcc4a69df616
ilmira khalilova <mirakhaleesi@yandex.ru>
2023-10-19 17:19:42 +0300

    first

---
+++ controllers/users.js
@@ -0,0 +1,95 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const NotFoundError = require('../errors/not-found-err');
+const ConflictError = require('../errors/conflict-error');
+const IncorrectDataError = require('../errors/incorrect-data-error');
+const UnauthorizedError = require('../errors/unauthorized-error');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+// возвращает информацию о пользователе (email и имя)
+module.exports.getUserInfo = (req, res, next) => {
+  User.findById(req.user._id)
+  // User.findById('652ef11ed95e0b9a216d3ef9')
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному _id не найден.');
+      }
+      res.send(user);
+    })
+    .catch(next);
+};
+
+module.exports.editUser = (req, res, next) => { // обновляет информацию о пользователе (email и имя)
+  const { email, name } = req.body;
+  User.findByIdAndUpdate(req.user._id, { email, name }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному _id не найден.');
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new IncorrectDataError('Переданы некорректные данные при обновлении профиля.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+// создаёт пользователя с переданными в теле email, password и name
+module.exports.createUser = (req, res, next) => {
+  const {
+    email,
+    password,
+    name,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email,
+      password: hash,
+      name,
+    }))
+    .then((user) => res.status(201).send({
+      email: user.email,
+      name: user.name,
+    }))
+    .catch((err) => {
+      if (err.code === 11000) {
+        next(new ConflictError('Пользователь с таким email уже зарегистрирован.'));
+      } else if (err.name === 'ValidationError') {
+        next(new IncorrectDataError('Переданы некорректные данные при создании пользователя.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+// проверяет переданные в теле почту и пароль и возвращает JWT
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Неправильные почта или пароль.');
+      }
+      bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            // хеши не совпали — отклоняем промис
+            throw new UnauthorizedError('Неправильные почта или пароль.');
+          }
+          // аутентификация успешна
+          const token = jwt.sign(
+            { _id: user._id },
+            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+            { expiresIn: '7d' },
+          );
+          res.status(200).send({ token }); // .cookie('jwt', token, { httpOnly: true })
+        })
+        .catch(next);
+    })
+    .catch(next);
+};
---
+++ middlewares/validation.js
@@ -0,0 +1,47 @@
+const { celebrate, Joi } = require('celebrate');
+
+const urlRegex = /https?:\/\/(\w{3}\.)?[1-9a-z\-.]{1,}\w\w(\/[1-90a-z.,_@%&?+=~/-]{1,}\/?)?#?/;
+
+module.exports.createUserValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+  }),
+});
+
+module.exports.loginValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+module.exports.editUserValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+module.exports.createMovieValidation = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().regex(urlRegex),
+    trailerLink: Joi.string().required().regex(urlRegex),
+    thumbnail: Joi.string().required().regex(urlRegex),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+module.exports.deleteMovieValidation = celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().required().hex().length(24),
+  }),
+});
