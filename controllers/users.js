7201255cad174b384ffce90b2c382f0d36cca23e
ilmira khalilova <mirakhaleesi@yandex.ru>
2023-10-19 22:13:25 +0300

    bag fix controller users

--- controllers/users.js
+++ controllers/users.js
@@ -31,8 +31,10 @@ module.exports.editUser = (req, res, next) => { // обновляет инфор
       res.send(user);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new IncorrectDataError('Переданы некорректные данные при обновлении профиля.'));
+      if (err.code === 11000) {
+        next(new ConflictError('Пользователь с таким email уже зарегистрирован.'));
+      } else if (err.name === 'ValidationError') {
+        next(new IncorrectDataError('Переданы некорректные данные при создании пользователя.'));
       } else {
         next(err);
       }
